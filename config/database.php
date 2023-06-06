7dc03354155cdb80cd198a51f76c792dd74a8a15
ianMuchesia <ianmuchesia@yahoo.com>
2023-06-06 08:28:12 +0300

    first commit

---
+++ config/database.php
@@ -0,0 +1,19 @@
+<?php
+
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'Ian');
+    define('DB_PASS', 'password');
+    define('DB_NAME', 'php_dev');
+
+
+    //create connection
+    $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+    //check connection
+    if($conn -> connect_error){
+        die('Connection Failed' . $conn->connect_error);
+    }
+ 
+    echo 'CONNECTED';
+
+    
