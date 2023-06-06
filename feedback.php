9dce01905ed1826edecce9df82c226e611c4eeca
ianMuchesia <ianmuchesia@yahoo.com>
2023-06-06 08:51:04 +0300

    array of objects in PHP

--- feedback.php
+++ feedback.php
@@ -2,24 +2,52 @@
 
 include 'inc/header.php'; ?>
    
-    <h2>Feedback</h2>
+   <?php
+   $feedback = 
+   [
+    [
+      'id'=> '1',
+      'name' => 'Jonnel Ceazer',
+      'email' => 'Jonnel@email.com',
+      'body'=> 'Data Structures and Algorithms'
+          ],
+          [
+            'id'=> '2',
+            'name' => 'Jacuzi Mapozi',
+            'email' => 'Jacuzi@email.com',
+            'body'=> 'Fundamentals of Data Science'
+          ],
+          [
+            'id'=> '1',
+            'name' => 'Typescript',
+            'email' => 'type@email.com',
+            'body'=> 'Typescript and Javascript'
+          ]
+    ];
 
-    <div class="card my-3">
-     <div class="card-body">
-       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
+   ?>
+    <h2> Past Feedback</h2>
+    <?php
+      if(empty($feedback)):
+    ?>
+        <p class="lead mt-3">There are no past feedback.</p>
+    <?php endif; ?>
+
+
+
+
+    <?php 
+    foreach($feedback  as $item): ?>
+    <div class="card my-3 w-75">
+     <div class="card-body text-center">
+      <?php echo $item['body']; ?>
+      <div class="text-secondary mt-2">
+        By <?php echo $item['name'];?>
+      </div>
      </div>
    </div>
+   <?php endforeach; ?>
 
-   <div class="card my-3">
-     <div class="card-body">
-       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
-     </div>
-   </div>
-
-   <div class="card my-3">
-     <div class="card-body">
-       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
-     </div>
-   </div>
+  
    <?php include 'inc/footer.php'; ?>
 
